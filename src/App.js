b6ec8c2053b87b169b12f571c18114aeb9ee38b8
Daniel <danielrs9504@gmail.com>
2021-09-13 10:50:24 -0500

    Elimina referencias a react en el App component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,7 @@
-function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React from 'react';
+
+const App = () => {
+  return <div>triki</div>;
+};
 
 export default App;
