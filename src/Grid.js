02c560f34957561b4afd231e57ca703ba796269d
Daniel <danielrs9504@gmail.com>
2021-09-13 13:09:02 -0500

    Agrega logica para calcular el ganador y muestra si ya se ha ganado o no

--- src/Grid.js
+++ src/Grid.js
@@ -9,6 +9,9 @@ const Grid = () => {
   // Definimos el turno
   const [turn, setTurn] = useState(true);
 
+  // calculate winner
+  const playerWinner = winner(squares);
+
   // Pasa la logica del renderizado del Square a una funcion
   const renderSquare = (i) => {
     return (
@@ -45,8 +48,37 @@ const Grid = () => {
         {renderSquare(7)}
         {renderSquare(8)}
       </div>
+      <div>
+        {playerWinner ? `The winner is: ${playerWinner}` : <>No winner yet!</>}
+      </div>
     </div>
   );
+};
+
+const winner = (squares) => {
+  const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < winningCombos.length; i++) {
+    // Iteramos por las posibles combinaciones, cuando i=0 => [a,b,c] = [0,1,2]
+    const [a, b, c] = winningCombos[i];
+
+    // Si el turno en cada uno de los squares es iguales en la combinacion ganadora definida anteriormente
+    // Es decir, si evaluamos nuestro puntaje (square) en las posicion ganadora i=0 y los 3 valores son iguales a X u O, es ganador
+    // En otras palabras, si en la combinacion ganadora, los 3 valores son iguales hay un ganador
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 };
 
 export default Grid;
