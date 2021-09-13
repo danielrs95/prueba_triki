6660c6cd99e85c35db76823aaa0bc6cde311e836
Daniel <danielrs9504@gmail.com>
2021-09-13 10:53:03 -0500

    Cambia nombre de App por Grid

--- src/Grid.js
+++ src/Grid.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const App = () => {
+const Grid = () => {
   return <div>triki</div>;
 };
 
-export default App;
+export default Grid;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Grid from './Grid';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Grid />
   </React.StrictMode>,
   document.getElementById('root')
 );
