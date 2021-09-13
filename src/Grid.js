9b2edeb8d95ba34b6a98c2692baefec69ab70d17
Daniel <danielrs9504@gmail.com>
2021-09-13 12:36:09 -0500

    Extrae la logica del renderizado del Square y crea el estado inicial de los square

--- src/Grid.js
+++ src/Grid.js
@@ -1,23 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Square from './Square';
 
 const Grid = () => {
+  // Los cuadros van a tener 3 estados, null, X u O.
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  console.log(squares);
+
+  // Pasa la logica del renderizado del Square a una funcion
+  const renderSquare = (i) => {
+    return <Square value={i} onClick={null} />;
+  };
+
   return (
     <div className='container mt-5'>
       <div className='row'>
-        <Square value={0} onClick={null} />
-        <Square value={1} onClick={null} />
-        <Square value={2} onClick={null} />
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
       </div>
       <div className='row'>
-        <Square value={3} onClick={null} />
-        <Square value={4} onClick={null} />
-        <Square value={5} onClick={null} />
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
       </div>
       <div className='row'>
-        <Square value={6} onClick={null} />
-        <Square value={7} onClick={null} />
-        <Square value={8} onClick={null} />
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
       </div>
     </div>
   );
